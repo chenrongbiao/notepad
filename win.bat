a97f4672aa2aae1778398f3566bb73175f693eb5
XiaoPb <lzh175603@163.com>
2023-02-06 23:15:08 +0800

    优化打包流程

--- win.bat
+++ win.bat
@@ -1,16 +1,5 @@
-cd build
-if exist %1 (
-    @echo "%1 文件夹已存在"
-) else (
-    mkdir %1
-)
-cd %1
-if exist plugin (
-    @echo "plugin 文件夹已存在"
-) else (
-    mkdir plugin
-)
-cd ..
-windeployqt notepad--.exe --dir=%1
+cd build/bin
+
+windeployqt notepad--.exe
 @REM copy .\src\qscint\libqscint.a .\%1\plugin
 @REM copy .\src\plugin\^*\^*.dll .\%1\plugin
