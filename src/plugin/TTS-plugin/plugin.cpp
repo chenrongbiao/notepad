c19efdcff9522ce456665530242b3b295738f501
zinface <zinface@163.com>
2023-04-26 01:16:35 +0800

    cmake-plugin: 调整 TTS-plugin 插件适配可扩展宏方案

--- src/plugin/TTS-plugin/plugin.cpp
+++ src/plugin/TTS-plugin/plugin.cpp
@@ -6,6 +6,20 @@
 #include "ndd_plugin_implement.h"
 
 
-NOTEPAD_PLUGIN_METADATA_IDENTIFY_V1(u8"TTS 语音合成", "0.1", "zinface", u8"二级菜单高级插件", "");
+NOTEPAD_PLUGIN_METADATA_IDENTIFY_1 {
+    USE_IDENTIFY_VARIABLES
 
-NOTEPAD_PLUGIN_METADATA_IMPLEMENT_V1(NddPluginImplement, false);
+    NOTEPAD_PLUGIN_METADATA(u8"TTS 语音合成", "0.1", "zinface", u8"二级菜单高级插件", "");
+
+    return true;
+}
+
+NOTEPAD_PLUGIN_METADATA_IMPLEMENT_1 {
+    USE_IMPLEMENT_VARIABLES
+
+    auto imp = new NddPluginImplement(s_pNotepad, s_getCurEdit());
+    imp->setCurrentEditFunc(s_getCurEdit);
+    imp->setMenuActions(s_pProcData.m_rootMenu);
+
+    return 0;
+}
