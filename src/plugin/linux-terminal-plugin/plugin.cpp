cbd8d0e97b2f841e98c3c4e6170835998d3e47ad
zinface <zinface@163.com>
2023-04-25 21:56:16 +0800

    cmake-plugin: 调整 linux-terminal 插件适配可扩展宏方案

--- src/plugin/linux-terminal-plugin/plugin.cpp
+++ src/plugin/linux-terminal-plugin/plugin.cpp
@@ -5,7 +5,20 @@
 #include <qsciscintilla.h>
 #include "ndd_plugin_implement.h"
 
-NOTEPAD_PLUGIN_METADATA_IDENTIFY_V1("Linux Terminal", "0.1", "author", u8"基于 LXQT QTerminal 的插件", "");
+NOTEPAD_PLUGIN_METADATA_IDENTIFY_1 {
+    USE_IDENTIFY_VARIABLES
 
+    NOTEPAD_PLUGIN_METADATA("简易 Linux 集成终端", "0.1", "author", u8"基于 LXQT QTerminal 的插件", "");
 
-NOTEPAD_PLUGIN_METADATA_IMPLEMENT_V1(NddPluginImplement, false);
+    return true;
+}
+
+NOTEPAD_PLUGIN_METADATA_IMPLEMENT_1 {
+    USE_IMPLEMENT_VARIABLES
+
+    auto imp = new NddPluginImplement(s_pNotepad, s_getCurEdit());
+    imp->setMenuActions(s_pProcData.m_rootMenu);
+    imp->setCurrentEditFunc(s_getCurEdit);
+
+    return 0;   
+}
