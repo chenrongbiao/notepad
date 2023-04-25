505bb25c0ed75c03800a6fd6e6dfb6850c29235e
zinface <zinface@163.com>
2023-04-25 23:50:37 +0800

    cmake-plugin: 调整 external-plugin 插件适配可扩展宏方案

--- src/plugin/external-plugin/plugin.cpp
+++ src/plugin/external-plugin/plugin.cpp
@@ -6,6 +6,20 @@
 #include "ndd_plugin_implement.h"
 
 
-NOTEPAD_PLUGIN_METADATA_IDENTIFY_V1(u8"外部插件扩展测试", "0.1", "zinface", u8"基于 QMainWindow Ui 的插件", "");
+NOTEPAD_PLUGIN_METADATA_IDENTIFY_1 {
+    USE_IDENTIFY_VARIABLES
 
-NOTEPAD_PLUGIN_METADATA_IMPLEMENT_V1(NddPluginImplement, false);
+    NOTEPAD_PLUGIN_METADATA(u8"外部插件扩展测试", PLUGIN_VERSION, "zinface", u8"基于 QMainWindow Ui 的插件", "")
+
+    return true;
+}
+
+NOTEPAD_PLUGIN_METADATA_IMPLEMENT_1 {
+    USE_IMPLEMENT_VARIABLES
+
+    auto imp = new NddPluginImplement(s_pNotepad, s_getCurEdit());
+    imp->setCurrentEditFunc(s_getCurEdit);
+    imp->setMenuActions(s_pProcData.m_rootMenu);
+
+    return 0;
+}
